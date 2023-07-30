62409eccca1419a8efb22600bf4131282ae186c4
lvheng <1834805770@qq.com>
2023-07-30 16:15:42 +0800

    Add ESLint

---
+++ rollup.config.ts
@@ -0,0 +1,8 @@
+export default {
+  output: {
+    preserveModules: true,
+    dir: 'dist',
+    format: 'es',
+    exports: 'auto'
+  }
+}
---
+++ src/reduce.ts
@@ -0,0 +1,5 @@
+function reduce(a: number, b: number) {
+  return a - b
+}
+
+export default reduce
