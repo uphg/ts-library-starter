3de2327cc9282d7e1b0043105372bf11b29d9ea8
lvheng <1834805770@qq.com>
2025-07-20 09:58:06 +0800

    chore: remove test file

--- test-file.js
+++
@@ -1,9 +0,0 @@
-// 这是一个测试文件，有格式问题
-const testVar = 'double quotes'
-const obj = { a: 1, b: 2 }
-function testFunc() {
-  return testVar
-}
-
-// 使用变量避免 unused-vars 错误
-console.log(testVar, obj, testFunc())
